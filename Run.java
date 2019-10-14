9af2f373b4a0ff2c1405d8afbf0b89912fe092c1
Atkins <atkinsx@egb080000013.lancs.local>
2019-10-14 11:16:56 +0100

    Restarted project and created empty classes

---
+++ Run.java
@@ -0,0 +1,7 @@
+public class Run
+{
+    public static void main (String[] args)
+    {
+        SystemSimulation newSystem = new SystemSimulation(600,600);
+    }
+}
---
+++ SystemSimulation.java
@@ -0,0 +1,7 @@
+public class SystemSimulation extends SolarSystem
+{
+    public SystemSimulation(int width, int height)
+    {
+        super(width, height);
+    }
+}
